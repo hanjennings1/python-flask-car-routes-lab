ae57bbe11ea9e9ac78748ec29a39a6fb91e36045
Hannah Jennings <han.jennings1@gmail.com>
2026-09-14 09:39:10 -0400

    Set up / route

--- server/app.py
+++ server/app.py
@@ -1,5 +1,15 @@
+# Import Flask
 from flask import Flask
 
+# Create the Flask app instance
 app = Flask(__name__)
 
+# Car models currently in the fleet
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+
+# Handle GET requests to "/"
+@app.route('/')
+def index():
+    # Return plain text as the response
+    return 'Welcome to Flatiron Cars'

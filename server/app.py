f70250b5ac2feb1233acfacd8af767d6d96f99d9
Hannah Jennings <han.jennings1@gmail.com>
2026-09-14 09:45:34 -0400

    Add /<model> route

--- server/app.py
+++ server/app.py
@@ -13,3 +13,12 @@ existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
 def index():
     # Return plain text as the response
     return 'Welcome to Flatiron Cars'
+
+# Handle GET requests to "/<model>"
+@app.route('/<model>')
+def get_model(model):
+    # Check if requested model exists in fleet
+    if model in existing_models:
+        return f'Flatiron {model} is in our fleet!'
+    else:
+        return f'No models called {model} exists in our catalog'
